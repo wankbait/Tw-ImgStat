944b51c0232bdb635c2d66b41a318d72e98f4cff
Koi Hamilton <hamiltonkoi5@gmail.com>
2019-11-20 19:21:29 -0500

    Remove redundant CSVHelper hints

--- ImgStat/Tweet.cs
+++ ImgStat/Tweet.cs
@@ -9,25 +9,15 @@ using CsvHelper.Configuration.Attributes;
 //Small tweet object to write to CSV (using Tweetinvi.Models.ITweet produced a stack overflow within VS, and was too slow anyway)
 public class Tweet
 {
-    [Index(0)]
     public string ID { get; }
-    [Index(1)]
     public int Fav { get; }
-    [Index(2)]
     public int RT { get; }
-    [Index(3)]
     public int Followers { get; }
-    [Index(4)]
     public int? Replies { get; }
-    [Index(5)]
     public DateTime CreationTime { get; }
-    [Index(6)]
     public string Content { get; }
-    [Index(7)]
     public string MediaUrl { get; }
-    [Index(8)]
     public string TweetUrl { get; } //Should this be removed to preserve anonymity? Or will it not matter because it will not be included in the paper?
-    [Index(9)]
     public float LikeFollowRatio { get; }
 
     public Tweet(Tweetinvi.Models.ITweet tweet)
@@ -44,12 +34,15 @@ public class Tweet
         {
             try
             {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(e);
+                Console.ForegroundColor = ConsoleColor.White;
                 this.MediaUrl = tweet.ExtendedTweet.ExtendedEntities.Medias[0].MediaURL;
             }
             catch (Exception e2)
             {
-                Console.Error.WriteLine("FOR SOME REASON THERE IS NO IMAGE LINK IN THIS IMAGE TWEET \n GO YELL AT TWITTER FOR THIS I GUESS");
-                throw new Exception();
+                Console.Error.WriteLine($"FOR SOME REASON THERE IS NO IMAGE LINK IN THIS IMAGE TWEET \n GO YELL AT TWITTER FOR THIS I GUESS \n \nSTACK TRACE: {e2}");
+                throw e2;
                 //TODO: handle this
             }
         }
