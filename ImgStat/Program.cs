600936745625d1c12adb26d2fc8214a8ef2f6c71
Koi Hamilton <hamiltonkoi5@gmail.com>
2020-01-02 01:20:24 -0500

    write to an output file to be updated with subsequent program runs & read by excel

--- ImgStat/Program.cs
+++ ImgStat/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
+using NReco.Csv;
 
 namespace ImgStat
 {
@@ -16,21 +17,55 @@ namespace ImgStat
             TweetGrabber.Init();
             FileMgr.Init();
 
-            //TweetGrabber.Fetch(100);
-            //TweetGrabber.Download();
+            TweetGrabber.Fetch(250);
+            TweetGrabber.Download();
 
             ImgParser imgParser = new ImgParser();
             var downloads = Directory.EnumerateFiles(FileMgr.DLPath);
             int count = 1;
             
-            Parallel.ForEach(downloads, file =>
+            //Parallel.ForEach(downloads, file =>
+            //{
+            //    Console.Write("\n" + count + ": ");
+            //    count++;
+            //    Console.WriteLine($"STAT:{file}\n");
+            //    imgParser.GetStat(file);
+            //});
+            using (StreamWriter streamWriter = new StreamWriter(FileMgr.OutFile))
             {
-                Console.Write("\n" + count + ": ");
-                count++;
-                Console.WriteLine($"STAT:{file}\n");
-                imgParser.GetStat(file);
-            });
+                Parallel.ForEach(Directory.EnumerateFiles(FileMgr.CSVPath), filePath =>
+                {
+                    using (StreamReader streamReader = new StreamReader(filePath))
+                    {
+                        CsvReader csvReader = new CsvReader(streamReader);
+                        CsvWriter csv = new CsvWriter(streamWriter);
+                        while (csvReader.Read())
+                        {
+                            csv.WriteField(csvReader[0]);
+                            csv.WriteField(csvReader[1]);
+                            csv.WriteField(csvReader[2]);
+                            csv.WriteField(csvReader[3]);
+                            csv.WriteField(csvReader[4]);
+                            csv.WriteField(csvReader[5]);
+                            csv.WriteField(csvReader[6]);
+                            csv.WriteField(csvReader[7]);
+                            csv.WriteField(csvReader[8]);
 
+                            var stat = imgParser.GetStat(FileMgr.DLPath + $"{csvReader[0]}.jpg");
+                            //Mean Saturation
+                            csv.WriteField(stat.MeanSat.ToString());
+                            //Write record to file
+                            csv.NextRecord();
+                            Console.Write($"\n Wrote record {csvReader[0]} to {FileMgr.OutFile}");
+                        }
+                        count++;
+
+                        Console.WriteLine("Done.");
+
+                    }
+                });
+            }
+                
             Console.WriteLine($"AVG TIME: {ImgParser.totalElapsedMS / count}ms");
             //while (true)
             //{
